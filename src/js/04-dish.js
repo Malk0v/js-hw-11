3d0c33a5aaaee5be663328938a8d5229f646eac8
Malk0v <106482398+Malk0v@users.noreply.github.com>
2023-02-17 22:42:38 +0200

    Create 04-dish.js

---
+++ src/js/04-dish.js
@@ -0,0 +1,45 @@
+const btn = document.querySelector('ul');
+const text = document.querySelector('.userArea');
+
+btn.addEventListener('click', (e) => {
+  if (e.target.nodeName !== 'BUTTON') {
+    return;
+  }
+
+  text.innerHTML = '';
+
+  makeOrder(e.target.textContent).then(onMakeOrderSuccess).catch(onMakeOrderError);
+  // console.log(btn.textContent);
+
+  // text.insertAdjacentHTML('beforeend', e.target.textContent);
+
+});
+
+
+const makeOrder = dish => {
+  const DELAY = 1000;
+
+  return (promice = new Promise((resolve, reject) => {
+    const passed = Math.random() > 0.5;
+
+    setTimeout(() => {
+      if (passed) {
+        resolve(`😀 вот ваше блюдо, "${dish}"`);
+      }
+      reject(`😓 извините закончились продукты для, "${dish}", повторите чуть попозже`);
+    }, DELAY);
+  }));
+};
+
+// makeOrder('пирожок').then(onMakeOrderSuccess).catch(onMakeOrderError);
+
+function onMakeOrderSuccess(result) {
+  text.insertAdjacentHTML('beforeend', result);
+  console.log(result);
+}
+
+function onMakeOrderError(error) {
+  text.insertAdjacentHTML('beforeend', error);
+  console.log(error);
+}
+ 
